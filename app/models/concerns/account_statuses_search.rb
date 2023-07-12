0e068e392737ee6081c07d4cdc295fca932b7577
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-12 12:48:10 -0400

    Add new publicly_searchable attr. to status index

---
+++ app/models/concerns/account_statuses_search.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module AccountStatusesSearch
+    extend ActiveSupport::Concern
+  
+    def update_statuses_index!
+      puts "AccountStatusesSearch::update_statuses_index!"
+      Chewy.strategy(:atomic) do
+        StatusesIndex.import(query: Status.where(account_id: id))
+      end
+    end
+  end
