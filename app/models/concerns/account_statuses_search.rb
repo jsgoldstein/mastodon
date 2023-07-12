13378d02d8d33a16e1a6c04d972f73b1cee24753
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-12 12:51:23 -0400

    get rid of debug line

--- app/models/concerns/account_statuses_search.rb
+++ app/models/concerns/account_statuses_search.rb
@@ -4,7 +4,6 @@ module AccountStatusesSearch
     extend ActiveSupport::Concern
   
     def update_statuses_index!
-      puts "AccountStatusesSearch::update_statuses_index!"
       Chewy.strategy(:atomic) do
         StatusesIndex.import(query: Status.where(account_id: id))
       end
