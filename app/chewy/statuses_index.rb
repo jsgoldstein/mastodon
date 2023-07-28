390e212330a20f10f3dc420c8594e5f796393b68
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-28 16:45:05 -0400

    Add a new public status index

--- app/chewy/statuses_index.rb
+++ app/chewy/statuses_index.rb
@@ -1,65 +1,79 @@
 # frozen_string_literal: true
 
+module BaseStatusIndexConfig
+  def base_status_index_settings
+    settings index: { refresh_interval: '30s' }, analysis: {
+      filter: {
+        english_stop: {
+          type: 'stop',
+          stopwords: '_english_',
+        },
+        english_stemmer: {
+          type: 'stemmer',
+          language: 'english',
+        },
+        english_possessive_stemmer: {
+          type: 'stemmer',
+          language: 'possessive_english',
+        },
+      },
+      analyzer: {
+        content: {
+          tokenizer: 'uax_url_email',
+          filter: %w(
+            english_possessive_stemmer
+            lowercase
+            asciifolding
+            cjk_width
+            english_stop
+            english_stemmer
+          ),
+        },
+      },
+    }
+  end
+
+  def base_status_index_crutches
+    {
+      mentions: lambda do |collection|
+        data = ::Mention.where(status_id: collection.map(&:id)).where(account: Account.local, silent: false).pluck(:status_id, :account_id)
+        data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+      end,
+
+      favourites: lambda do |collection|
+        data = ::Favourite.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
+        data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+      end,
+
+      reblogs: lambda do |collection|
+        data = ::Status.where(reblog_of_id: collection.map(&:id)).where(account: Account.local).pluck(:reblog_of_id, :account_id)
+        data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+      end,
+
+      bookmarks: lambda do |collection|
+        data = ::Bookmark.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
+        data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+      end,
+
+      votes: lambda do |collection|
+        data = ::PollVote.joins(:poll).where(poll: { status_id: collection.map(&:id) }).where(account: Account.local).pluck(:status_id, :account_id)
+        data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+      end
+    }
+  end
+end
+
 class StatusesIndex < Chewy::Index
+  extend BaseStatusIndexConfig
   include FormattingHelper
 
-  settings index: { refresh_interval: '30s' }, analysis: {
-    filter: {
-      english_stop: {
-        type: 'stop',
-        stopwords: '_english_',
-      },
-      english_stemmer: {
-        type: 'stemmer',
-        language: 'english',
-      },
-      english_possessive_stemmer: {
-        type: 'stemmer',
-        language: 'possessive_english',
-      },
-    },
-    analyzer: {
-      content: {
-        tokenizer: 'uax_url_email',
-        filter: %w(
-          english_possessive_stemmer
-          lowercase
-          asciifolding
-          cjk_width
-          english_stop
-          english_stemmer
-        ),
-      },
-    },
-  }
+  settings base_status_index_settings
 
   # We do not use delete_if option here because it would call a method that we
   # expect to be called with crutches without crutches, causing n+1 queries
   index_scope ::Status.unscoped.kept.without_reblogs.includes(:media_attachments, :preloadable_poll)
-
-  crutch :mentions do |collection|
-    data = ::Mention.where(status_id: collection.map(&:id)).where(account: Account.local, silent: false).pluck(:status_id, :account_id)
-    data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
-  end
-
-  crutch :favourites do |collection|
-    data = ::Favourite.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
-    data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
-  end
-
-  crutch :reblogs do |collection|
-    data = ::Status.where(reblog_of_id: collection.map(&:id)).where(account: Account.local).pluck(:reblog_of_id, :account_id)
-    data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
-  end
-
-  crutch :bookmarks do |collection|
-    data = ::Bookmark.where(status_id: collection.map(&:id)).where(account: Account.local).pluck(:status_id, :account_id)
-    data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
-  end
-
-  crutch :votes do |collection|
-    data = ::PollVote.joins(:poll).where(poll: { status_id: collection.map(&:id) }).where(account: Account.local).pluck(:status_id, :account_id)
-    data.each.with_object({}) { |(id, name), result| (result[id] ||= []).push(name) }
+  base_status_index_crutches.each do |crutch_name, crutch_block|
+    crutch crutch_name, &crutch_block
   end
 
   root date_detection: false do
@@ -71,5 +85,35 @@ class StatusesIndex < Chewy::Index
     end
 
     field :searchable_by, type: 'long', value: ->(status, crutches) { status.searchable_by(crutches) }
+  end
+end
+
+class PublicStatusesIndex < Chewy::Index
+  extend BaseStatusIndexConfig
+  include FormattingHelper
+
+  settings base_status_index_settings
+
+  # We do not use delete_if option here because it would call a method that we
+  # expect to be called with crutches without crutches, causing n+1 queries
+  index_scope ::Status.unscoped
+                      .kept
+                      .without_reblogs
+                      .includes(:media_attachments, :preloadable_poll)
+                      .joins(:account)
+                      .where(accounts: { discoverable: true })
+                      .where(visibility: :public)
+
+  base_status_index_crutches.each do |crutch_name, crutch_block|
+    crutch crutch_name, &crutch_block
+  end
+
+  root date_detection: false do
+    field :id, type: 'long'
+    field :account_id, type: 'long'
+
+    field :text, type: 'text', value: ->(status) { status.searchable_text } do
+      field :stemmed, type: 'text', analyzer: 'content'
+    end
   end
 end
