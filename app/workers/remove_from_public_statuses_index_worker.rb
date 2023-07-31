749cd543c1b1eed4f48d9132a5fd7428552a0ceb
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-31 13:46:23 -0400

    Just trying to make the build happy again...

--- app/workers/remove_from_public_statuses_index_worker.rb
+++ app/workers/remove_from_public_statuses_index_worker.rb
@@ -1,16 +1,5 @@
 # frozen_string_literal: true
 
-class AddToPublicStatusIndexWorker
-  include Sidekiq::Worker
-
-  def perform(account_id)
-    account = Account.find(account_id)
-    return unless account&.discoverable?
-
-    account.add_to_public_statuses_index!
-  end
-end
-
 class RemoveFromPublicStatusesIndexWorker
   include Sidekiq::Worker
 
