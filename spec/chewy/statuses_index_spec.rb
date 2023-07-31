9c07d74a22ec3c033a3a3d10569d986c36dd85c5
jsgoldstein <jakegoldstein95@gmail.com>
2023-07-31 11:49:57 -0400

    Move test to the right spot and try agian

--- spec/chewy/statuses_index_spec.rb
+++ spec/chewy/statuses_index_spec.rb
@@ -29,3 +29,45 @@ describe StatusesIndex do
     }
   end
 end
+
+RSpec.describe StatusesIndex do
+  it 'has settings defined' do
+    expect(StatusesIndex).to respond_to(:settings)
+    expect(StatusesIndex.settings).to be_a(Chewy::Index::Settings)
+  end
+end
+
+describe PublicStatusesIndex do
+  describe 'Searching the index' do
+    before do
+      mock_elasticsearch_response(described_class, raw_response)
+    end
+
+    it 'returns results from a query' do
+      results = described_class.query(match: { name: 'status' })
+
+      expect(results).to eq []
+    end
+  end
+
+  def raw_response
+    {
+      took: 3,
+      hits: {
+        hits: [
+          {
+            _id: '0',
+            _score: 1.6375021,
+          },
+        ],
+      },
+    }
+  end
+end
+
+RSpec.describe PublicStatusesIndex do
+  it 'has settings defined' do
+    expect(PublicStatusesIndex).to respond_to(:settings)
+    expect(PublicStatusesIndex.settings).to be_a(Chewy::Index::Settings)
+  end
+end
